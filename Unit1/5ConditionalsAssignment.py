43ad771de8c903d207abd706464660a4db52e756
Cooper <cooperashley@usd266.com>
2024-04-10 12:29:30 -0500

    did some workk...

--- Unit1/5ConditionalsAssignment.py
+++ Unit1/5ConditionalsAssignment.py
@@ -1,29 +1,34 @@
 
-print("########## 1.5.1 ##########")
+# print("########## 1.5.1 ##########")
+# number = int(input("please type number:"))
+# if number == 1984:
+#     print("orwell")
+# print("########## 1.5.2 ##########")
+# number = int(input("please type in a number:"))
+# if number < 0:
+#     print("the absolute value of that number is", number * -1)
+# print("########## 1.5.3 ##########")
+name = input("what is your name?")
 
-print("########## 1.5.2 ##########")
+# print("########## 1.5.4 ##########")
 
-print("########## 1.5.3 ##########")
+# print("########## 1.5.5 ##########")
 
-print("########## 1.5.4 ##########")
+# print("########## 1.5.6 ##########")
 
-print("########## 1.5.5 ##########")
+# print("########## 1.5.7 ##########")
 
-print("########## 1.5.6 ##########")
+# print("########## 1.5.8 ##########")
+# # Fix the program
+# points = int(input("How many points are on your card? "))
+# if points < 100:
+#     points *= 1.1
+#     print("Your bonus is 10 %")
 
-print("########## 1.5.7 ##########")
+# if points >= 100:
+#     points *= 1.15
+#     print("Your bonus is 15 %")
 
-print("########## 1.5.8 ##########")
-# Fix the program
-points = int(input("How many points are on your card? "))
-if points < 100:
-    points *= 1.1
-    print("Your bonus is 10 %")
+# print("You now have", points, "points")
 
-if points >= 100:
-    points *= 1.15
-    print("Your bonus is 15 %")
-
-print("You now have", points, "points")
-
-print("########## 1.5.9 ##########")
+# print("########## 1.5.9 ##########")
