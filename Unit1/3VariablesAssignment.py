73abc5c23a5a16044cc5f8d6a3708c86d0435d50
Cooper <cooperashley@usd266.com>
2024-04-10 12:01:46 -0500

    did 1.3 and 1.4

--- Unit1/3VariablesAssignment.py
+++ Unit1/3VariablesAssignment.py
@@ -1,34 +1,38 @@
-print("########## 1.3.1 ##########")
+# print("########## 1.3.1 ##########")
 
-name = "Tim Tester"
-age = 20
-skill1 = "python"
-level1 = "beginner"
-skill2 = "java"
-level2 = "veteran"
-skill3 = "programming"
-level3 = "semiprofessional"
-lower = 2000
-upper = 3000
+# name = "Tim Tester"
+# age = 20
+# skill1 = "python"
+# level1 = "beginner"
+# skill2 = "java"
+# level2 = "veteran"
+# skill3 = "programming"
+# level3 = "semiprofessional"
+# lower = 2000
+# upper = 3000
 
-print("my name is ", name, " , I am ", age, "years old")
-print("my skills are")
-print("- ", skill1, " (", level1, ")")
-print("- ", skill2, " (", level2, ")")
-print("- ", skill3, " (", level3, " )")
-print("I am looking for a job with a salary of", lower, "-", upper, "dollars per month")
+# print("my name is ", name, " , I am ", age, "years old")
+# print("my skills are")
+# print("- ", skill1, " (", level1, ")")
+# print("- ", skill2, " (", level2, ")")
+# print("- ", skill3, " (", level3, " )")
+# print("I am looking for a job with a salary of", lower, "-", upper, "dollars per month")
 
 
-print("########## 1.3.2 ##########")
-x = 27
-y = 15
+# print("########## 1.3.2 ##########")
+# x = 4
+# y = 9
 
+# print(x, "+", y, "=", x + y)
+# print(x, "-", y, "=", x - y)
+# print(x, "*", y, "=", x * y)
+# print(x, "/", y, "=", x / y)
 
 print("########## 1.3.3 ##########")
-print(5)
-print(" + ")
-print(8)
-print(" - ")
-print(4)
+print(5, end="")
+print()
+print(8,end="")
+print()
+print(4, end="")
 print(" = ")
-print(5 + 8 - 4)
+print(5 + 8 - 4, end="")
--- Unit1/4ArithmeticAssignment.py
+++ Unit1/4ArithmeticAssignment.py
@@ -1,23 +1,52 @@
 print("########## 1.4.1 ##########")
-
+digit = int (input("please type a number:"))
+print(digit, "*", "5", "=", digit*5)
 print("########## 1.4.2 ##########")
+name = input("What is your name?")
+year = int(input("Which year were you born?"))
+age = 2024-year
+print("Hi", name, "you will be", age, "years old at the end of 2024.")
+
 
 print("########## 1.4.3 ##########")
+days = int(input("how many days?"))
+print ("secomds in that many days:", days*86400)
 
 print("########## 1.4.4 ##########")
 # Fix the code
-number = int(input("Please type in the first number: "))
-number = int(input("Please type in the second number: "))
-number = int(input("Please type in the third number: "))
+number1 = int(input("Please type in the first number: "))
+number2 = int(input("Please type in the second number: "))
+number3 = int(input("Please type in the third number: "))
 
-product = number * number * number
+product = number1 * number2 * number3
 
 print("The product is", product)
 
 print("########## 1.4.5 ##########")
-
+number4 = int(input("please type in number:"))
+number5 = int(input("please type in number:"))
+sum = number4 + number5
+product = number4 * number5
+print("the sum is, ", sum)
+print("the product is:", product)
 print("########## 1.4.6 ##########")
-
+number1 = int(input("please type in first number:"))
+number2 = int(input("please type in second number:"))
+number3 = int(input("please type in third number:"))
+number4 = int(input("please type in fourth number:"))
+sum = number1 + number2 + number3 + number4
+mean = (number1 + number2 + number3 + number4) / 4
+print("the sum of the numbers is ", sum, "and the mean is ", mean)
 print("########## 1.4.7 ##########")
-
+times_eaten = float(input("How many times a week do you eat at the student cafeteria?"))
+price_of_food = float(input("What is the price of a typical student lunch?"))
+groceries = float(input("How much do you spend on groceries in a week?"))
+daily = (times_eaten * price_of_food + groceries) / 7
+weekly = times_eaten * price_of_food + groceries
+print("Average food expenditure:")
+print("Daily:", daily)
+print("Weekly:", weekly)
 print("########## 1.4.8 ##########")
+number_in_course = int(input("How many students on the course?"))
+number_in_group = int(input("desired group size?"))
+print("Number of groups formed", number_in_course // number_in_group)
