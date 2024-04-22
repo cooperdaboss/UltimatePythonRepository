cb4838b23331752b1dd0389cdb26328bb43ca532
Cooper <cooperashley@usd266.com>
2024-04-22 12:19:53 -0500

    finished 2.2

--- Unit2/2CompoundConditionsAssignment.py
+++ Unit2/2CompoundConditionsAssignment.py
@@ -1,11 +1,53 @@
 # print("########## 2.2.1 ##########")
-
+# age = int(input("What is your age? "))
+# if age >= 4:
+#     print("ok, you're ", age, "years old")
+# if age <=3 and age >=1:
+#     print("i suspect you can't write quite yet...")
+# if age <= 0:
+#     print("that must be a mistake")
 # print("########## 2.2.2 ##########")
+# name = input("please type in your name:")
+# if name == "Morty" or name == "Ferdie":
+#     print("I think you might be one of Mickey Mouse's nephews.")
+
+# if name == "Huey" or name == "Dewey" or name == "Louie":
+#     print("I think you might be one of Donald Duck's nephews.")
+
+# if name != "Morty" and name != "Ferdie" and name != "Huey" and name != "Dewey" and name != "Louie":
+#    print("You're not a nephew of any character i know of.")
+
+# if name == "Morty" or name == "Ferdie":
+#     print("I think you might be one of Mickey Mouse's nephews.")
+# elif name == "Huey" or name == "Dewey" or name == "Louie":
+#     print("I think you might be one of Donald Duck's nephews.")
+# else:
+#    print("You're not a nephew of any character i know of.")
 
 # print("########## 2.2.3 ##########")
+# grade = int(input("Please type in percentage:"))
+# if grade <= 100 and grade >= 90:
+#     print("Grade: A")
+# elif grade <= 89 and grade >= 80:
+#     print("grade: B")
+# elif grade <= 79 and grade >= 70:
+#     print("Grade: C")
+# elif grade <= 69 and grade >= 60:
+#     print("Grade: D")
+# elif grade <= 59:
+#     print("Grade: F")
+# elif grade >= 101:
+#     print("impossible!")
 
 # print("########## 2.2.4 ##########")
+# number = int(input("Please type in a number:"))
+# if number % 3 == 0 and number % 5 == 0:
+#     print("FizzBuzz")
+# elif number % 5 == 0:
+#     print("Buzz")
+# elif number % 3 == 0:
+#     print("Fizz")
 
 # print("########## 2.2.5 ##########")
-
+year = int(input("please type in a year:"))
 # print("########## 2.2.6 ##########")
---
+++ scratch.py
@@ -0,0 +1,17 @@
+age = int(input("Please enter your age"))
+
+
+if age == 18:
+    print("Happy first voting day")
+elif age >= 18:
+    print("You can vote")
+else:
+    print("You're too young to vote")
+
+
+# if age >= 18:
+#     print("You can vote")
+# elif age == 18:
+#     print("Happy first voting day")
+# else:
+#     print("You're too young to vote")
