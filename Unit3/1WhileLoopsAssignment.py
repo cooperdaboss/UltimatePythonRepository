2b88b9eb3971bee70f08668860bead9c43192ac3
Cooper <cooperashley@usd266.com>
2024-04-30 12:54:05 -0500

    finished 3.1

--- Unit3/1WhileLoopsAssignment.py
+++ Unit3/1WhileLoopsAssignment.py
@@ -1,21 +1,56 @@
 # ========== 3.1.1 ==========
+# number = 0
+# while True:
+#     number += 2
+#     print(number)
+#     if number == 30:
+#         break
 
-
-# ========== 3.1.2 ==========
+# # ========== 3.1.2 ==========
 
 # print("Are you ready?")
 # number = int(input("Please type in a number: "))
-# while number = 0:
-# print(number)
-# print("Go!")
+# while True:
+#     print(number)
+#     number = number - 1
+#     if number == 0:
+#         break
+# print("Now!")
 
 # ========== 3.1.3 ==========
-
-
+# number = int(input("Upper limit:"))
+# newnumber = 1
+# while True:
+#     print(newnumber)
+#     newnumber += 1
+#     if newnumber == number:
+#         break
 # ========== 3.1.4 ==========
-
+# number = int(input("Upper limit:"))
+# newnumber = 1
+# while True:
+#     print(newnumber)
+#     newnumber *= 2
+#     if newnumber > number:
+#         break
 
 # ========== 3.1.5 ==========
-
+# number = int(input("Upper limit:"))
+# newnumber = int(input("Base:"))
+# startnumber = 1
+# while True:
+#     print(startnumber)
+#     startnumber *= newnumber
+#     if startnumber > number:
+#         break
 
 # ========== 3.1.6 ==========
+# number = int(input("Limit:"))
+# consecutivenumber = 0
+# curent = 0
+# while True:
+#     curent = curent + 1
+#     consecutivenumber = consecutivenumber + curent
+#     if consecutivenumber >= number:
+#         break
+# print(consecutivenumber)
