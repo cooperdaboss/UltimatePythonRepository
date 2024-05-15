35bab60a5d09398f542674836ad2e356eaaac4f6
Cooper <cooperashley@usd266.com>
2024-05-15 11:58:32 -0500

    finished 4.1

--- Unit4/1PrintingFunctionsAssignment.py
+++ Unit4/1PrintingFunctionsAssignment.py
@@ -1,19 +1,75 @@
 # ========== 4.1.1 ==========
+# def line(inte, two):
+
+#     if two == "":
+#         print("*" * inte)
+#     elif len(two) >= 2:
+#         print(two[0] * inte)
+#     else:
+#         print(two * inte)
 
 
+# line(7, "%")
+# line(10, "LOL")
+# line(3, "")
 # ========== 4.1.2 ==========
-
+# def box_of_hashes(number):
+#     counter = 1
+#     while True:
+#         print("#" * 10)
+#         counter = counter +1
+#         if counter == number + 1:
+#             break
+# box_of_hashes(2)
 
 # ========== 4.1.3 ==========
-
+# def square_of_hashes(number):
+#     counter = 1
+#     while True:
+#         print("#" * number)
+#         counter = counter + 1
+#         if counter == number +1:
+#             break
+# square_of_hashes(3)
 
 # ========== 4.1.4 ==========
-
-
+# def square(number, text):
+#     counter = 1
+#     while True:
+#         print(text * number)
+#         counter = counter + 1
+#         if counter == number + 1:
+#             break
+# square(5, "*")
+# square(3, "o")
 # ========== 4.1.5 ==========
-
+# def triangle(number):
+#     counter = 1
+#     while True:
+#         print("#" * counter)
+#         counter = counter + 1
+#         if counter == number + 1:
+#             break
+# triangle(3)
 
 # ========== 4.1.6 ==========
-
-
+# def shape(num1, shape1, num2, shape2):
+#     counter = 1
+#     while True:
+#         if counter == num1 + 1:
+#             break
+#         print(shape1 * counter)
+#         counter = counter +1 
+        
+#     counter2 = 0
+#     while True:
+#         if counter2 == num2:
+#             break
+#         print(shape2 * num1)
+#         counter2 = counter2 + 1
+        
+# shape(5, "X", 3, "*")
+# shape(2, "o", 4, "+")
+# shape(3, ".", 0, ",")
 # ========== 4.1.7 ==========
+    
