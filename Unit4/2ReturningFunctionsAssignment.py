a6d45a0cbf8b9be7f3c248f47d0a2365ab96f539
Cooper <cooperashley@usd266.com>
2024-05-16 12:08:01 -0500

    finshed most of 4.2

---
+++ Unit4/2ReturningFunctionsAssignment.py
@@ -0,0 +1,30 @@
+# ========== 4.2.1 ==========
+# def greatest_number(num1, num2, num3):
+
+#     if num1 > num2 and num1 > num3:
+#         return(num1)
+#     elif num2 > num1 and num2 > num3:
+#         return(num2)
+#     else: return(num3)
+
+# print(greatest_number(3, 4, 1))
+# print(greatest_number(99, -4, 7))
+# print(greatest_number(0, 0, 0))
+# ========== 4.2.2 ==========
+# def same_chars(string, num1, num2):
+#     letter1 = string[num1]
+#     letter2 = string[num2]
+
+#     print("letter 1 is", letter1)
+#     print("letter 2 is", letter2)
+#     if letter1 == letter2:
+#         return("true")
+#     else:
+#         return("false")
+
+# print(same_chars("programmer", 6, 7))
+# print(same_chars("programmer", 0, 4))
+# ========== 4.2.3 ==========
+
+
+
